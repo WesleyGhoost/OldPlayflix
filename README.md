c3a29d930122f720d5652dacef23b8c610ca4406
Wesley <wesley.felix12@hotmail.com>
2022-09-15 19:45:58 -0300

    play

---
+++ README.md
@@ -0,0 +1,4 @@
+# PlayFlix
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/_Ghoost_/pen/GRyqEqB](https://codepen.io/_Ghoost_/pen/GRyqEqB).
+
---
+++ src/script.js
@@ -0,0 +1,16 @@
+var listaGames = [
+  "https://cdn-prod.scalefast.com/public/assets/user/122595/image/4ea88a5395a3618cb13479f6b5172f79.png",
+"https://cdn.shopify.com/s/files/1/0747/3829/products/mL2363_1024x1024.jpg?v=1571445542",
+  "https://orig00.deviantart.net/e3b5/f/2010/106/9/c/half_life_movie_poster_by_rockmassif.png",
+  "https://th.bing.com/th/id/R.002f7e50ee7cc5295c494aa039df54ba?rik=yuVHrwVdoCQPUg&riu=http%3a%2f%2fgameplaying.info%2fwp-content%2fuploads%2f2016%2f06%2fhollow-knight-poster.jpg&ehk=UCmvkclkP44Z6P3WwoDfjelZBeLL5%2b5Z9Wqm%2f%2b05ZCo%3d&risl=&pid=ImgRaw&r=0",
+"https://i.pinimg.com/originals/48/69/1b/48691bd8d3c858955068843b69954512.jpg",
+  "https://i5.walmartimages.com/asr/7c5ed218-04e0-4ee2-a91a-2f62612590cf_1.26759cf8c09943623525d5d3dec7c7c5.jpeg",
+  "https://d1x7zurbps6occ.cloudfront.net/product/xlarge/607886-171224.jpg",
+  "https://th.bing.com/th/id/OIP.thWHTBRmDOfzHu_ZTH8ayQHaKm?pid=ImgDet&rs=1",
+  "https://th.bing.com/th/id/OIP.Qtqe2vw3bryDYU1GXtqymgAAAA?pid=ImgDet&rs=1",
+"https://th.bing.com/th/id/R.7a6803af5a033f7e25999693ebeee9fa?rik=z%2bvan%2b3j8bSG2w&riu=http%3a%2f%2fwww.merchandisingplaza.com%2f127458%2f2%2fPosters-The-Last-Of-Us-Poster-The-Last-Of-Us-Black-and-White-Portrait-l.jpg&ehk=b8qBnUkceq5Nu2P6iMd6Ef3UiFmKTuTkdu4GqRDZGEY%3d&risl=&pid=ImgRaw&r=0",
+];
+for (var o = 0; o < 10; o++) {
+  document.write("<img src=" + listaGames[o] + ">");
+}
+
